0c312c3f8471e9f1f1cd2ecc346e0c1784dc6f0b
mattig3090 <36996707+mattig3090@users.noreply.github.com>
2020-04-21 01:58:30 -0400

    Added comment to student.js about Auth

--- models/students.js
+++ models/students.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const Student = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
 });
+
+// we'll want a way to be able to pull information from the signin to be able to update this
 
 module.exports = mongoose.model('Student', Student);
