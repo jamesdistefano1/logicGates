f3e55269cc2a791a30d8db98eb6df827290f286a
Stephen Moss <smoss@udel.edu>
2020-04-22 11:59:42 -0400

    wtf am I doing lol, need to rethink a lot of things that I tried doing, stable base to do so from

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Draggable from 'react-draggable';
+import Canvas from './modules/application/canvas/Canvas.js';
+import Selection from './modules/application/selection/Selection.js';
 
 function App(){
 
   return(
-    //<h1>Project</h1>
-    <div id="canvas" style={{height: '50%', width: '90%', position: 'relative', overflow: 'auto', padding: '0'}}>
-      <Draggable grid={[50, 50]}>
-        <img src={require('./logo.svg')} draggable = "false"/> 
-      </Draggable>
+    <div>
+      <Canvas />
+      <Selection />
     </div>
   );
 }
--- src/modules/application/canvas/Canvas.js
+++ src/modules/application/canvas/Canvas.js
@@ -5,9 +5,9 @@ import Draggable from 'react-draggable';
 function Canvas(){
 
   return(
-    <div id="canvas" style={{height: '50%', width: '90%', position: 'relative', overflow: 'auto', padding: '0'}}>
+    <div id="canvas" style={{height: '500px', width: '90%', position: 'relative', overflow: 'auto',  padding: '0'}}>
       <Draggable grid={[50, 50]}>
-        <img src={require('./logo.svg')} draggable = "false"/> 
+        <img src={require('./2input_base.svg')} draggable = "false" width = '100' height = '50'/> 
       </Draggable>
     </div>
   );
