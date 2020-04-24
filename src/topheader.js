776d28ee0a23dde1c14d990aac845b8b032abacd
dtolaoye <dtolaoye@udel.edu>
2020-04-23 23:16:51 -0400

    web-layout 2

---
+++ src/topheader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./topheader.css";
+
+
+
+function Topper(props){
+    return(
+        <div>
+            <div class="header">
+                <a href="#default" class="logo">Logic Gates Project</a>
+                    </div>
+        </div>
+    )
+}
+    
+
+export default Topper;
+
+
+
