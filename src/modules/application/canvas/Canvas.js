1bf726bb33006ca29239cc4abbc41e21f094eeff
Stephen Moss <smoss@udel.edu>
2020-04-30 13:33:38 -0400

    SVGs done

--- src/modules/application/canvas/Canvas.js
+++ src/modules/application/canvas/Canvas.js
@@ -7,7 +7,7 @@ function Canvas(){
   return(
     <div id="canvas" style={{height: '500px', width: '90%', position: 'relative', overflow: 'auto',  padding: '0'}}>
       <Draggable grid={[50, 50]}>
-        <img src={require('./2input_base.svg')} draggable = "false" width = '100' height = '50'/> 
+        <img src={require('./OR.svg')} draggable = "false" width = '100' height = '50'/> 
       </Draggable>
     </div>
   );
