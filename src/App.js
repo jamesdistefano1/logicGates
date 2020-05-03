5877bb0705f167683bf4e13227d34330a11ba01b
Stephen Moss <smoss@udel.edu>
2020-05-03 00:03:51 -0400

    Formatting is fucked for now but it's merged ish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import Tweet from './tewwt'
 import Table from './table'
 import Header from './header'
-=======
 import ReactDOM from 'react-dom';
 import Draggable from 'react-draggable';
 import Canvas from './modules/application/canvas/Canvas.js';
 import Selection from './modules/application/selection/Selection.js';
->>>>>>> Stephen1
 
 function App(){
 
